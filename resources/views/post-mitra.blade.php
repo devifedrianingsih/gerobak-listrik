540f0c1c729214d604a43907ca6dfc9203af6e0a
Devi Fedrianingsih <devifedrianingsih@apps.ipb.ac.id>
2024-12-03 01:26:36 +0700

    post mitra

---
+++ resources/views/post-mitra.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hubungi Kami</title>
+    <link rel="stylesheet" href="{{ asset('css/form-mitra.css') }}">
+    <script src="https://kit.fontawesome.com/4a1b340fe5.js" crossorigin="anonymous"></script>
+</head>
+<body> 
+    <div class="contact-container">
+        <h2>Data Anda telah dikirim</h2>
+        <div class="text">
+            <p>Mohon menunggu verifikasi berkas untuk dapat bergabung kemitraan.</p>
+            <p>Admin akan menghubungi Anda paling lambat 3 hari kerja.</p>
+            <p>Ada pertanyaan lebih lanjut?</p>
+        </div>
+        <div class="button-container">
+            <a href="/hubungikami" class="hubungi-button">Hubungi Kami</a>
+        </div>
+        <div class="button-container">
+            <a href="/beranda" class="back-button">Kembali ke Beranda</a>
+        </div>
+    </div>
+</body>
+</html>
